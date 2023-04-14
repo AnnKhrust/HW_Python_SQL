96682d04cab58b969fcca0dac00611e41e572a36
AnnKhrust <anna.khrust@gmail.com>
2023-04-15 00:12:34 +0300

    solve tasks

---
+++ functions.py
@@ -0,0 +1,89 @@
+import psycopg2
+
+def create_db(conn):
+    conn.execute("""
+        DROP TABLE IF EXISTS phonebook;
+        DROP TABLE IF EXISTS customer CASCADE;
+        """)
+    
+    conn.execute("""
+        CREATE TABLE IF NOT EXISTS customer(
+            customer_id SERIAL PRIMARY KEY,
+            first_name VARCHAR (40) NOT NULL,
+            last_name VARCHAR(40) NOT NULL,
+            email VARCHAR(40) UNIQUE 
+            );
+        """)
+    
+    conn.execute("""
+        CREATE TABLE IF NOT EXISTS phonebook(
+            customer_id INTEGER REFERENCES customer(customer_id),
+            phone INTEGER UNIQUE CHECK(phone >= 0)
+            );
+        """)
+
+def add_customer(conn, first_name, last_name, email, phones=None):
+    conn.execute("""
+    INSERT INTO customer (first_name, last_name, email) VALUES(%s, %s, %s);
+    """, (first_name, last_name, email))
+
+def add_phone(conn, customer_id, phone):
+    conn.execute("""
+    INSERT INTO phonebook (customer_id, phone) VALUES(%s, %s);
+    """, (customer_id, phone))
+
+def change_customer(conn, customer_id, first_name=None, last_name=None, email=None, phone=None):
+    conn.execute("""
+        UPDATE customer SET first_name=%s WHERE customer_id=%s
+        """, (first_name, customer_id))
+       
+    conn.execute("""
+        UPDATE customer SET last_name=%s WHERE customer_id=%s
+        """, (last_name, customer_id))
+    
+    conn.execute("""
+        UPDATE customer SET email=%s WHERE customer_id=%s
+        """, (email, customer_id))
+    
+    conn.execute("""
+        UPDATE phonebook SET phone=%s WHERE customer_id=%s
+        """, (customer_id, phone))
+
+def delete_phone(conn, phone):
+    conn.execute("""
+    DELETE FROM phonebook WHERE phone=%s;
+    """, (phone,))
+
+def delete_customer(conn, customer_id):
+    conn.execute("""
+    DELETE FROM phonebook WHERE customer_id=%s;
+    """, (customer_id,))
+
+    conn.execute("""
+    DELETE FROM customer WHERE customer_id=%s;
+    """, (customer_id,))
+
+def find_customer(conn, first_name=None, last_name=None, email=None, phone=None):
+    conn.execute("""
+        SELECT cl.first_name, cl.last_name, cl.email, ph.phone FROM customer AS cl
+        LEFT JOIN phonebook AS ph ON cl.customer_id = ph.customer_id
+        WHERE first_name=%s;
+        """, (first_name,))
+    
+    conn.execute("""
+        SELECT cl.first_name, cl.last_name, cl.email, ph.phone FROM customer AS cl
+        LEFT JOIN phonebook AS ph ON cl.customer_id = ph.customer_id
+        WHERE last_name=%s;
+        """, (last_name,))
+    
+    conn.execute("""
+        SELECT cl.first_name, cl.last_name, cl.email, ph.phone FROM customer AS cl
+        LEFT JOIN phonebook AS ph ON cl.customer_id = ph.customer_id
+        WHERE email=%s;
+        """, (email,))
+    
+    conn.execute("""
+        SELECT cl.first_name, cl.last_name, cl.email, ph.phone FROM customer AS cl
+        LEFT JOIN phonebook AS ph ON cl.customer_id = ph.customer_id
+        WHERE phone=%s;
+        """, (phone,))
---
+++ main.py
@@ -0,0 +1,25 @@
+import psycopg2
+from signin import password, username
+from functions import *
+
+if __name__ == "__main__":
+    with psycopg2.connect(database="netology_hw_db", user=username, password=password) as conn:
+        with conn.cursor() as cur:
+            create_db(cur)
+            add_customer(cur, 'Vanda', 'Maksimof', 'red-witch@gmail.com')
+            add_customer(cur, 'Natalia', 'Romanof', 'blackW@yandex.ru')
+            add_customer(cur, 'Stiven', 'Strandg', 'the-doctor@gmail.com')
+            add_customer(cur, 'Anthony', 'Starck', 'genius@gmail.com')
+            conn.commit()
+            add_phone(cur, '1', '380904883')
+            add_phone(cur, '2', '79119111')
+            add_phone(cur, '4', '1000000000')
+            conn.commit()
+            change_customer(cur, '1', first_name="Wanda", last_name='Altron')
+            change_customer(cur, '4', first_name='Tony', last_name="Stark")
+            conn.commit()
+            find_customer(cur, first_name='Stiven')
+            conn.commit()
+            delete_phone(cur, phone='1000000000')
+            delete_customer(cur, '2')
+            conn.commit()
